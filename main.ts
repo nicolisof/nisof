eeedbd95ff3580b79cbca0f09bde610ff001a31b
nicolisof <114233847+nicolisof@users.noreply.github.com>
2022-09-23 09:40:24 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+let P2 = 0
+let P1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Lets go!")
+    basic.pause(randint(1000, 10000))
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    P2 += 1
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    P1 += 1
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
